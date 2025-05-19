4ce4c451817d8b48ccf1ed616cd2bb181a2e3a8c
Kacper Woś <kacper.wos33@gmail.com>
2025-05-19 17:35:12 +0200

    intro for variables

---
+++ 0_introduction/variables.py
@@ -0,0 +1,11 @@
+productName = "Smartphone"
+prductPrice = 1200
+
+print(productName + " after discount: ")
+print(prductPrice * 0.75 )
+
+topSpeed = 200
+topSPEED = 300
+
+print(topSpeed)
+print(topSPEED )
