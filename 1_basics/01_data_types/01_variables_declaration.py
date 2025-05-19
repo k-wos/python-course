134a44b89d80fda278252a74776b1a738aa2b1d8
Kacper Woś <kacper.wos33@gmail.com>
2025-05-19 17:44:37 +0200

    variables declarations

---
+++ 1_basics/01_data_types/01_variables_declaration.py
@@ -0,0 +1,22 @@
+number  = 100 
+
+print(number)
+
+number = 55
+
+print(number)
+
+number = "one"
+
+print(number)
+
+text = "Hello World!"
+
+print(text + "Hello again!")
+
+number = 70
+number2 = 10
+
+result = number + number2
+
+print(result)
