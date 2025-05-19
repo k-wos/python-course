cf06a92a17ee757a65597ffa3cfafa57bb0d8153
Kacper Woś <kacper.wos33@gmail.com>
2025-05-19 17:51:22 +0200

    variables task

---
+++ tasks/variables.py
@@ -0,0 +1,12 @@
+title = "Rezygnacja"
+content = "Rezygnuje z czegoś"
+sender = "KW"
+reciver = "AM"
+emailDate = "20.01.2000"
+
+
+print(title)
+print(content)
+print(sender)
+print(reciver)
+print(emailDate)
