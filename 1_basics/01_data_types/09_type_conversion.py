0d93701d76c169b1f78035eb976c4bd624111479
Kacper Woś <kacper.wos33@gmail.com>
2026-07-01 18:55:55 +0200

    type conversation

---
+++ 1_basics/01_data_types/09_type_conversion.py
@@ -0,0 +1,21 @@
+floatNum = 23.23123
+intNum = int(floatNum)
+print(type(intNum))
+print(intNum)
+
+print(int("678    "))
+
+intNum = 56
+float1 = float(intNum)
+print(type(float1))
+print(float1)
+
+
+str1 = "312.321312312"
+float2 = float(str1)
+print(type(float2))
+print(float2)
+
+
+print("Wartość float1: " + str(float1))
+
---
+++ tasks/type_conversion.py
@@ -0,0 +1,17 @@
+decimalNum = 45.6789
+wholeNum = int(decimalNum)
+print(type(wholeNum))
+print(wholeNum)
+
+str1 = "4321"
+print(type(int(str1)))
+print(int(str1))
+
+wholeNum2 = 123
+float1 = float(wholeNum2)
+print(type(float1))
+print(float1)
+
+str2 = "98.76"
+print(type(float(str2)))
+print(float(str2))
